6d908f9647846fc226f1bdc2464b5678b6a01648
fkibria <fkibria02@gmail.com>
2020-10-27 00:00:53 -0400

    initial commit

---
+++ quizquestions.js
@@ -0,0 +1,40 @@
+// create an array that contains objects that have question and answer choices
+
+var questions = [ {
+    
+    q: "Commonly used data types do NOT include",
+    choices: ["strings","booleans", "alerts","numbers"],
+    a: "alerts"
+
+},
+{
+
+    q: "The condition in an if / else statement is enclosed within ____.",
+    choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+    a: "parentheses"
+}, 
+{
+
+    q: "Arrays in Javascript can be used to store ____.",
+    choices: ["numbers and strings", "other arrays", "booleans", "all of the above"],
+    a: "all of the above"
+},
+{
+
+    q: "String values must be enclosed within ____ when being assigned to variables.",
+    choices: ["commas", "curly brackets", "quotes", "parenthesis"],
+    a: "quotes"
+},
+{
+    
+    q: "A very useful tool for used during development and debugging for printing content to the debugger is:",
+    choices: ["Javascript", "terminal / bash", "for loops", "console log"],
+    a: "console log"
+},
+
+];
+
+for (var i=0; i < choices.length; i++) {
+    
+}
+
