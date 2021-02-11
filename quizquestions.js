a3f95282d7d6b4d2611a9386b10678a13c0dcb0a
fkibria <fkibria02@gmail.com>
2021-02-11 14:28:40 -0500

    adding functions for right and wrong answers, gameover

--- quizquestions.js
+++ quizquestions.js
@@ -1,111 +1,104 @@
-// create an array that contains objects that have question and answer choices
-document.addEventListener("DOMContentLoaded",function(){
+let counter  = 75;
+var myInterval;
+const thePage = document.querySelector("#firstQuestion");
 
-
-
-
-var questions = [ {
-    
+const questions = [{
     q: "Commonly used data types do NOT include",
-    choices: ["strings","booleans", "alerts","numbers"],
+    choices: ["strings", "booleans", "alerts", "numbers"],
     a: "alerts"
-
 },
 {
-
     q: "The condition in an if / else statement is enclosed within ____.",
     choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
     a: "parentheses"
-}, 
+},
 {
-
     q: "Arrays in Javascript can be used to store ____.",
     choices: ["numbers and strings", "other arrays", "booleans", "all of the above"],
     a: "all of the above"
 },
 {
-
     q: "String values must be enclosed within ____ when being assigned to variables.",
     choices: ["commas", "curly brackets", "quotes", "parentheses"],
     a: "quotes"
 },
 {
-    
     q: "A very useful tool used during development and debugging for printing content to the debugger is:",
     choices: ["Javascript", "terminal / bash", "for loops", "console log"],
     a: "console log"
-},
-
+}
 ];
 
-var questionLength = questions.length;
+var questionIndex = questions.length;
 
+// create an array that contains objects that have question and answer choices
+document.addEventListener("DOMContentLoaded", function () {
+    var startScreen = document.querySelector(".start-screen");
+    var startButton = document.querySelector("#start");
 
-var myTimer = function(counter){
-    var myInterval = setInterval(function(){
-        if(counter==0){
+    startButton.addEventListener("click", () => {Start(startScreen, startButton);});
+})
+
+function Start(startScreen, startButton) {
+    startScreen.classList.add("hidden");
+    startButton.classList.add("hidden");
+    questionFunctionality();
+    myTimer();
+}
+
+var myTimer = function () {
+    myInterval = setInterval(function () {
+        if (counter <= 0) {
             clearInterval(myInterval)
-         }
-         else {
+            gameOver(0);
+        }
+        else {
             counter--;
-            document.getElementById("test").innerText = counter;
-         };
+            document.getElementById("timer").innerText = counter;
+        };
     }, 1000);
 }
 
-var questionFunctionality = function(){ 
-    var score = 0;
+function gameOver(finalScore){
+    thePage.innerHTML = `Game over your score is ${finalScore}`
+    document.getElementById("timer").innerText = "";
+}
 
-    var questions = [ {
-    
-        q: "Commonly used data types do NOT include",
-        choices: ["strings","booleans", "alerts","numbers"],
-        a: "alerts"
-    
-    },
-    {
-    
-        q: "The condition in an if / else statement is enclosed within ____.",
-        choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
-        a: "parentheses"
-    }, 
-    {
-    
-        q: "Arrays in Javascript can be used to store ____.",
-        choices: ["numbers and strings", "other arrays", "booleans", "all of the above"],
-        a: "all of the above"
-    },
-    {
-    
-        q: "String values must be enclosed within ____ when being assigned to variables.",
-        choices: ["commas", "curly brackets", "quotes", "parentheses"],
-        a: "quotes"
-    },
-    {
-        
-        q: "A very useful tool used during development and debugging for printing content to the debugger is:",
-        choices: ["Javascript", "terminal / bash", "for loops", "console log"],
-        a: "console log"
-    },
-    
-    ];
+const questionFunctionality = function () {
+    if (questionIndex == 0) {
+        clearInterval(myInterval)
+        gameOver(counter);
+    }
+    else {
+        questionIndex--;
+        console.log('question length', questionIndex);
+        console.log( questions[questionIndex].q);
+        thePage.innerHTML = `<h3>${questions[questionIndex].q}</h3>`;
+        displayChoices(questions[questionIndex].choices, thePage, questions[questionIndex].a);
+    };
+}
 
+function wrongAnswer(){
+    counter -= 10;
+    questionFunctionality();
+}
 
-       if(questionLength==0){
-            clearInterval(newInterval)
-         }
-         else {
-            questionLength--;
-            console.log('question length',questionLength);
-             document.getElementById("test2").innerHTML = questions[questionLength].q;
-             myTimer(5);
- 
-         };
- }
+function displayChoices(arr, dom, answer){
+    let myHtml = '';
+    for(let i = 0; i < arr.length; i++){
+        if(arr[i] === answer){
+            dom.innerHTML += '<button class="choiceRight">' + arr[i] + '</button>';
+        }else{
+            dom.innerHTML += '<button class="choice">' + arr[i] + '</button>';
+        }
+    }
 
-questionFunctionality();
-var newInterval = setInterval( questionFunctionality, 5000);
+    const wrongAnswers = document.querySelectorAll(".choice");
+    const rightAnswer = document.querySelector(".choiceRight");
 
+    wrongAnswers.forEach((btn) =>{
+        btn.addEventListener("click", wrongAnswer);
+    })
 
-
-})
+    rightAnswer.addEventListener("click", questionFunctionality);
+}
